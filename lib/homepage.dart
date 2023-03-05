a28940159c9dc751b3bfb419b1c969a679de0883
Pranjal Dahal <pranjaldahal@Pranjals-MacBook-Air.local>
2023-03-05 15:43:26 +0545

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,70 @@
+import 'dart:convert';
+
+import 'package:api/Models/posts_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:http/http.dart' as http;
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  List<Postmodel> postList = [];
+  Future<List<Postmodel>> getPostApi() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map i in data) {
+        postList.add(Postmodel.fromJson(i));
+      }
+      return postList;
+    } else {
+      return postList;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("API", style: TextStyle(color: Colors.red)),
+      ),
+      body: Column(children: [
+        Expanded(
+          child: FutureBuilder(
+              future: getPostApi(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Text("Loading");
+                } else {
+                  return ListView.builder(
+                      itemCount: postList.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                            child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                  'Title\n' + postList[index].title.toString()),
+                              Text('Description\n' +
+                                  postList[index].body.toString()),
+                            ],
+                          ),
+                        ));
+                      });
+                }
+              }),
+        )
+      ]),
+    );
+  }
+}
